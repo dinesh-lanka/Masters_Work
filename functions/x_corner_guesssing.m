2efd1b4db8185f25c911b60135eb5d0225dc7d07
dinesh-lanka <dinesh-lanka@users.noreply.github.com>
2017-07-12 19:54:57 +0200

    Changes made to corner guessing
    The corner guessing is modified to generate corner guesses for all the images using the 1st guess points.

--- functions/x_corner_guesssing.m
+++ functions/x_corner_guesssing.m
@@ -1,28 +1,45 @@
-function x_corner_guesssing(image,cascadeDetectorFilePath,initialcpsfilelocation,newcpsfilelocation)
+function x_corner_guesssing(folderPathforImages,startImage,endImage,cascadeDetectorFilePath,initialcpsfilelocation,newcpsfilelocation)
 %X_CORNER_GUESSSING Summary of this function goes here
 %   Detailed explanation goes here
 
-fileID=fopen(initialcpsfilelocation,'r');
-A=textscan(fileID,'%f %f');
-A=cell2struct(A,{'xcorner','ycorner'},2);
-noOfElements=numel(A.xcorner);
-
-% Writing the new guess points to another data file
-fileID2=fopen(newcpsfilelocation,'w');
-% figure, imshow(image);
-% Detecting the corners for all the markers
-for i=1:noOfElements
-    xcornerguess=A.xcorner(i);
-    ycornerguess=A.ycorner(i);
-    markerCornerGuess(i,:)=marker_detection(image,xcornerguess,ycornerguess,cascadeDetectorFilePath);
-    fprintf(fileID2,'%e\t %e\r\n',markerCornerGuess(i,:));
-%     hold on;
-%     plot(markerCornerGuess(i,1),markerCornerGuess(i,2),'g*');
-    clear markerCornerGuess;
+for i=startImage:endImage
+    %     Reading the total number of images in the folder and extracting the
+    %     image according to start and end image
+    fileType=fullfile(folderPathforImages,'*.BMP');
+    imageList=dir(fileType);
+    imageName=imageList(i).name;
+    image=imread([folderPathforImages imageName]);
+    
+    % Now reading the initial cps file from the initial cps file location and
+    % picking the file corresponding to the start image  selected
+    if (i-startImage)==0
+        cpsFilesList=dir(fullfile(initialcpsfilelocation,'*.dat'));
+        initialcpsfile=cpsFilesList(i).name;
+        initialcpsfile=[initialcpsfilelocation initialcpsfile];
+    end
+    fileID=fopen(initialcpsfile,'r');
+    A=textscan(fileID,'%f %f');
+    A=cell2struct(A,{'xcorner','ycorner'},2);
+    noOfElements=numel(A.xcorner);
+    
+    % Writing the new guess points to another data file
+    newcpsfile=[newcpsfilelocation 'cps_image_' num2str(i,'%06d') '.dat'];
+    fileID2=fopen(newcpsfile,'w');
+%     figure, imshow(image);
+    % Detecting the corners for all the markers
+    for j=1:noOfElements
+        xcornerguess=A.xcorner(j);
+        ycornerguess=A.ycorner(j);
+        markerCornerGuess(j,:)=marker_detection(image,xcornerguess,ycornerguess,cascadeDetectorFilePath);
+        fprintf(fileID2,'%e\t %e\r\n',markerCornerGuess(j,:));
+%         hold on;
+%         plot(markerCornerGuess(j,1),markerCornerGuess(j,2),'g*');
+        clear markerCornerGuess;
+    end
+    initialcpsfile=newcpsfile;
+    % Closing the files
+    fclose(fileID2);
+    fclose(fileID);
+    
 end
-% Closing the files
-fclose(fileID2);
-fclose(fileID);
-
 end
-
--- scripts/detect_markers_corners.m
+++ scripts/detect_markers_corners.m
@@ -6,11 +6,14 @@ warning('off','all');
 % Path of cascade trained file
 cascadeDetectorFilePath='E:\GitHub\Masters_Work\XML_Files\markerDetector.xml';
 % Reading the image
-image=imread('E:\GAC_Files\PW6glider\03_MEASUREMENT_images\cam_2\frame_000002.bmp');
+folderPathforImages='E:\GAC_Files\run_30\Cam2\';
 % Loading the initial cps guess points
-initialcpsfilelocation='E:\GAC_Files\PW6glider\cps\cam_2\cps_img00001.dat';
+initialcpsfilelocation='E:\GAC_Files\run_30\cps\cam_2\';
 % Writing the new guess points to another data file
-newcpsfilelocation='E:\GAC_Files\Test_Runs\Cobrawing\cps_image00001.dat';
+newcpsfilelocation='E:\GAC_Files\Test_Runs\Cobrawing\cam_2\';
+% Giving the images to be read
+startImage=1;
+endImage=720;
 % Detecting the corners for all the markers
-x_corner_guesssing(image,cascadeDetectorFilePath,initialcpsfilelocation,newcpsfilelocation);
+x_corner_guesssing(folderPathforImages,startImage,endImage,cascadeDetectorFilePath,initialcpsfilelocation,newcpsfilelocation);
 toc
