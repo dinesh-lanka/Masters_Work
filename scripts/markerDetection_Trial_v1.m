940084325ee291a90b8afd64e2d80e0b9191a510
dinesh-lanka <dinesh-lanka@users.noreply.github.com>
2017-07-24 00:19:57 +0200

    Minor Changes

--- scripts/markerDetection_Trial_v1.m
+++ scripts/markerDetection_Trial_v1.m
@@ -8,7 +8,7 @@ detector=vision.CascadeObjectDetector(cascadeDetectorFilePath);
 % detector.MergeThreshold=10;
 
 for i=1
-    filepath='E:\GAC_Files\Images\002.bmp';
+    filepath='E:\GAC_Files\Cobra_Propeller\Cobra_prop\pictures\cam_1\FT_034326.bmp';
     imagefile=filepath;
     %     imagefile=[filepath '\Images\' '009.BMP'];
     %     filename=[filepath '\Positive\' num2str(i, '%d') '.PNG'];
