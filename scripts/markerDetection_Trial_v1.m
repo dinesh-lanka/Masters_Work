3449bddc87cd8823aba80d29eb34f9b7145d9f71
dinesh-lanka <dinesh-lanka@users.noreply.github.com>
2017-09-16 09:39:51 +0200

    Improved Classifier

--- scripts/markerDetection_Trial_v1.m
+++ scripts/markerDetection_Trial_v1.m
@@ -1,5 +1,5 @@
-% clear;
-% clc;
+clear;
+clc;
 % cd 'C:\Users\JAI\Desktop\Markers\';
 folderPath='E:\GitHub\Masters_Work\';
 detectorFilename='XML_Files\markerDetectorAnyBackground.xml';
@@ -8,8 +8,9 @@ detector=vision.CascadeObjectDetector(cascadeDetectorFilePath);
 % detector.MergeThreshold=8;
 
 for i=1
+    filepath = 'E:\GAC_Files\Images_From_Test_Runs\Cobra_Propeller\Cobra_prop\pictures\cam_1\FT_000300.bmp';
 %     filepath='E:\GAC_Files\Images_From_Test_Runs\0001.tiff';
-    filepath='E:\GAC_Files\Images_From_Test_Runs\run_30\Cam1\Cam1RH_Seq30_11h22m18s725ms_0000.bmp';
+%     filepath='E:\GAC_Files\Images_From_Test_Runs\run_30\Cam1\Cam1RH_Seq30_11h22m18s725ms_0000.bmp';
     imagefile=filepath;
     %     imagefile=[filepath '\Images\' '009.BMP'];
     %     filename=[filepath '\Positive\' num2str(i, '%d') '.PNG'];
